031233a2a6b540af5468926b8a7db0bd49e162ef
Jakob Hellermann <hellermann@sipgate.de>
2025-12-01 11:09:36 +0100

    various changes

--- .config/fish/functions/clone.fish
+++ .config/fish/functions/clone.fish
@@ -1,5 +1,5 @@
 function clone -a url --description 'clone repo by url'
     set url (echo "$url" | cut -f 1-5 -d '/')
     echo "$url"
-    git clone (git-http-to-ssh "$url")
+    jj git clone (git-http-to-ssh "$url")
 end
--- .config/fish/functions/hx.fish
+++ .config/fish/functions/hx.fish
@@ -1,3 +1,3 @@
-function hx --wraps=helix --description 'alias hx helix'
+type -q helix && function hx --wraps=helix --description 'alias hx helix'
     helix $argv
 end
