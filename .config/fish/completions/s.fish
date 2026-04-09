557553a7a1d2bd76abf4640ee90b92d505dc5add
Jakob Hellermann <hellermann@sipgate.de>
2026-04-09 14:31:24 +0200

    sipgate

---
+++ .config/fish/completions/s.fish
@@ -0,0 +1,1 @@
+/Users/sipgatejj/dev/claude-playground/s-completions.fish
--- .config/fish/functions/cdsd.fish
+++ .config/fish/functions/cdsd.fish
@@ -1,3 +1,3 @@
 function cdsd --wraps='cd ~/dev/sipgate-deployment' --description 'alias cdsd cd ~/dev/sipgate-deployment'
-    cd ~/dev/sipgate-deployment $argv
+    cd ~/dev/other/sipgate-deployment $argv
 end
