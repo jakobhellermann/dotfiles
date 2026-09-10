09f1ca7e9c01c9325934c71d551a81d7239e7434
Jakob Hellermann <hellermann@sipgate.de>
2026-09-10 18:52:51 +0200

    fix dotnet telemetry optout

--- .config/fish/conf.d/vars.fish
+++ .config/fish/conf.d/vars.fish
@@ -4,4 +4,4 @@ set -g RUSTUP_TOOLCHAIN nightly
 set -xg FZF_DEFAULT_OPTS '--reverse --info hidden'
 set -xg ANDROID_HOME "$HOME/.android/sdk"
 
-set -g DOTNET_CLI_TELEMETRY_OPTOUT false
+set -xg DOTNET_CLI_TELEMETRY_OPTOUT false
